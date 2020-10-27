95cc3ba263d1c37c464cb2089907ba97808ae739
Arthur <71701993+Avonnar@users.noreply.github.com>
2020-10-27 15:03:06 +0600

    Update matrixblock.cpp

--- matrixblock.cpp
+++ matrixblock.cpp
@@ -1,45 +1,90 @@
-//Жолдасов Артур
 #include <iostream>
-#include <memory>
-#include <vector>
 #include <thread>
+using namespace std;
 
-void multiply(const int *m_1, const int *m_2,
-              std::size_t size, std::size_t row, std::size_t col, int *m_res)
+const int N = 4;
+
+int B[N][N], A[N][N], C[N][N];
+
+void threadFunction(int rows, int col)
 {
-    for (std::size_t i = 0; i < size; ++i)
-        m_res[row * size + col] += m_1[row * size + i] * m_2[i * size + col];
+
+    for (int i = rows; i < rows + N / 2; ++i)
+    {
+        for (int j = col; j < col + N / 2; ++j)
+        {
+            C[i][j] = 0;
+            for (int k = 1; k < N; ++k)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 }
 
 int main()
-{
-    constexpr int N = 4, M = 1, THREAD_NUM = 2;
-
-    std::unique_ptr<int[]> A(new int[N * N]{
-        1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4});
-    std::unique_ptr<int[]> B(new int[N * M]{
-        5, 6, 7, 8});
-    std::unique_ptr<int[]> C(new int[N * N]{});
-
-    // create vector for running threads then assign threads to its elements
-    std::vector<std::thread> thread_group(THREAD_NUM);
-
-    for (int thread_i = 0; thread_i < THREAD_NUM; ++thread_i)
-        thread_group[thread_i] = std::thread([&, thread_i]() {
-            for (int row = thread_i; row < N; row += THREAD_NUM)
-            {
-                for (int col = 0; col < N; ++col)
-                    multiply(A.get(), B.get(), N, row, col, C.get());
-            }
-        });
-
-    for (auto &t : thread_group)
-        t.join();
-
-    for (int i = 0; i < N; ++i)
+{ //матрица А
+    for (int i = 1; i < N + 1; i++)
     {
-        for (int j = 0; j < N; ++j)
-            std::cout << (j ? "\t" : "") << C[i * N + j];
-        std::cout << std::endl;
+        for (int j = 1; j < N + 1; j++)
+        {
+            cout << "A[" << i << "][" << j << "]=";
+            cin >> A[i][j];
+        }
     }
+    //матрица В
+    for (int i = 1; i < N + 1; i++)
+    {
+        for (int j = 1; j < N + 1; j++)
+        {
+            cout << "B[" << i << "][" << j << "]=";
+            cin >> B[i][j];
+        }
+    }
+    //матрица А
+    for (int i = 1; i < N + 1; i++)
+    {
+        for (int j = 1; j < N + 1; j++)
+        {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    //матрица B
+    for (int i = 1; i < N + 1; i++)
+    {
+        for (int j = 1; j < N + 1; j++)
+        {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "=" << endl;
+
+    thread thr1;
+    thr1 = thread(threadFunction, 0, 0);
+    thread thr2;
+    thr2 = thread(threadFunction, 0, N / 2);
+    thread thr3;
+    thr3 = thread(threadFunction, N / 2, 0);
+    thread thr4;
+    thr4 = thread(threadFunction, N / 2, N / 2);
+    thr1.join();
+    thr2.join();
+    thr3.join();
+    thr4.join();
+
+    //ВЫВОД C
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << C[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
 }
